b40f07e71a136a380047b099103c0f41c1545c94
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-11-13 16:43:25 +0500

    remove extra blank lines in dvmn_api.py for improved readability

--- dvmn_api.py
+++ dvmn_api.py
@@ -4,9 +4,7 @@ import requests
 
 DVMN_URL = 'https://dvmn.org/api/user_reviews/'
 DVMN_URL_LONG_POLLING = 'https://dvmn.org/api/long_polling/'
-
 LONG_POLLING_TIMEOUT_SECONDS = 95
-
 
 def fetch_reviews(timestamp, dvmn_token):
     headers = {
